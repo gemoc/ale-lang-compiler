b76a6784985087ea2fb2b3b81147f58253c9a531
manuel <manuel.leduc@gmail.com>
2018-11-19 15:00:49 +0100

    improved benchmark script

--- plzoo/benchmark.hs
+++ plzoo/benchmark.hs
@@ -1,5 +1,6 @@
 import           Control.Monad (liftM2)
 import           Data.Char     (toLower)
+import           Data.List     (intersperse)
 
 -- Datas
 data Language=Boa|Petrinet|Logo deriving (Show)
@@ -11,9 +12,9 @@ data Program = Program {
   name     :: String
 } deriving (Show)
 
-data Runtime=Graalvm|Java8|GraalvmTruffle deriving (Show)
+data Runtime=Graalvm|Java8|GraalvmTruffle|OpenJ9 deriving (Show)
 
-data CompileTarget=Visitor|Revisitor|Interpreter|Truffle deriving (Show)
+data CompileTarget=Visitor|Revisitor|Interpreter|Truffle|Switch deriving (Show)
 
 type Programs = [Program]
 
@@ -22,11 +23,10 @@ runtimeToVar :: Runtime -> String
 runtimeToVar Graalvm        = "$GRAALVM_HOME"
 runtimeToVar GraalvmTruffle = "$GRAALVM_HOME"
 runtimeToVar Java8          = "$JAVA8_HOME"
+runtimeToVar OpenJ9         = "$OPENJ9_HOME"
 
 flattenedtargets :: [(CompileTarget, Runtime)]
 flattenedtargets = concatMap (\(ct, rts) -> map (\x -> (ct,x)) rts) targets
-
-languages = [Logo] -- Boa, Petrinet,
 
 p :: Language -> String -> Program
 p = Program
@@ -37,15 +37,26 @@ combine = liftM2 (,)
 tojarName :: (Language, CompileTarget) -> String
 tojarName (l, ct) = map toLower $ show l ++ "_" ++ show ct ++ ".jar"
 
+fullname :: CompileTarget -> Runtime -> Program -> String
+fullname ct r p' = map toLower $ show ct ++ "_" ++ show r ++ "_" ++ show (language p') ++ "_" ++ name p'
+
 logname :: ((CompileTarget, Runtime), Program) -> String
-logname ((ct, r), p) = map toLower $ show ct ++ "_" ++ show r ++ "_" ++ show (language p) ++ "_" ++ name p ++ ".log"
+logname ((ct, r), p') = fullname ct r p' ++ ".log"
 
-toExecution :: (Num a, Show a) => ((CompileTarget, Runtime), Program) -> a -> String
-toExecution t@((ct, GraalvmTruffle), p) itt = runtimeToVar GraalvmTruffle ++ "/bin/java -XX:-UseJVMCIClassLoader -jar " ++ tojarName (language p, ct) ++ " " ++ name p ++ ".xmi" ++  " " ++ show itt ++ " "++ " > " ++ logname t
-toExecution t@((ct, r), p) itt = runtimeToVar r ++ "/bin/java -jar " ++ tojarName (language p, ct) ++ " " ++ name p ++ ".xmi" ++  " " ++ show itt ++ " "++ " > " ++ logname t
+toExecution :: ((CompileTarget, Runtime), Program) -> String
+toExecution t@((ct, GraalvmTruffle), p') = unlines [
+    "echo \"" ++ fullname ct GraalvmTruffle p' ++ "\""
+  , lastLineAndSave  command file ]
+  where command = runtimeToVar GraalvmTruffle ++ "/bin/java -XX:-UseJVMCIClassLoader -jar " ++ tojarName (language p', ct) ++ " " ++ name p' ++ ".xmi" ++  " " ++ "$ITERATIONS"
+        file = logname t
+toExecution t@((ct, r), p') = unlines [
+    "echo \"" ++ fullname ct r p' ++ "\""
+  , lastLineAndSave command file ]
+  where command = runtimeToVar r ++ "/bin/java -jar " ++ tojarName (language p', ct) ++ " " ++ name p' ++ ".xmi" ++  " " ++ "$ITERATIONS"
+        file = logname t
 
-toComment :: [Char] -> [Char]
-toComment a = "# " ++ a
+lastLineAndSave :: String -> String -> String
+lastLineAndSave command file = command ++ " | tail -n1 > " ++ file
 
 toCheckFile :: Language -> CompileTarget -> String
 toCheckFile ct r = unlines [
@@ -61,31 +72,65 @@ compilerTargets = [Visitor, Revisitor, Interpreter, Truffle]
 
 targets :: [(CompileTarget, [Runtime])]
 targets = [
-    (Visitor, [Graalvm, Java8])
-  , (Revisitor, [Graalvm, Java8])
-  , (Interpreter, [Graalvm, Java8])
+    (Visitor, [Graalvm, Java8, OpenJ9])
+  , (Revisitor, [Graalvm, Java8, OpenJ9])
+  , (Interpreter, [Graalvm, Java8, OpenJ9])
+  , (Switch, [Graalvm, Java8, OpenJ9])
   , (Truffle, [GraalvmTruffle])
   ]
 
+languages = [Logo, Boa] -- Boa, Petrinet,
 
--- TODO: add different kind of programes in BOA
 programs :: Programs
 programs = [
-  p Logo "fractal" -- p Boa "fib10", p Boa "fib20", p Petrinet "manyToOne", p Petrinet "oneToOne",
+  p Logo "fractal",
+  p Boa "fib10",
+  p Boa "fib20"
+  --, p Petrinet "manyToOne", p Petrinet "oneToOne",
   ]
 
+printResult :: ((CompileTarget, Runtime), Program) -> String
+printResult x@((ct, r), p') = unlines [
+    "echo -n \"" ++ fullname ct r p' ++ "=\""
+  , "tail -n1 " ++ logname x
+  ]
 
+results :: [((CompileTarget, Runtime), Program)] -> String
+results xs = "results = {" ++ unwords (intersperse "," $ map result xs) ++ "}"
+  where result ((ct, r), p')= "\""++ fn ct r p' ++"\": " ++ fn ct r p'
+        fn = fullname
+
+printResults :: [((CompileTarget, Runtime), Program)] -> String
+printResults x = unlines (map printResult x ++ ["echo "++show (results x)])
+
+setifnull :: String -> String -> String
+setifnull key value = "if [ -z ${" ++ key ++ "+x} ]; then " ++ key ++ "=" ++ value ++ "; fi"
+
+printkeyvalue :: String -> String
+printkeyvalue key = "echo -n " ++ key ++ "=; echo $" ++ key
+
+checkdirexists :: String -> String
+checkdirexists dir = "if [ ! -d \"$" ++ dir ++ "\" ]; then echo " ++ dir ++ "=$"++dir++" does not exist; exit 1 ; fi"
 
 main :: IO ()
 main = do
-  putStrLn "JAVA8_HOME=/usr/lib/jvm/java-8-oracle"
-  putStrLn "GRAALVM_HOME=$HOME/jdk/graalvm-ee-1.0.0-rc7"
+  putStrLn $ setifnull "JAVA8_HOME" "$PWD/java-8-oracle"
+  putStrLn $ setifnull "GRAALVM_HOME" "$PWD/graalvm-ee-1.0.0-rc7"
+  putStrLn $ setifnull "OPENJ9_HOME" "$PWD/openj9-9-0.11.0"
+  putStrLn $ setifnull "ITERATIONS" "100"
+  putStrLn $ checkdirexists "JAVA8_HOME"
+  putStrLn "echo SUMMARY:"
+  putStrLn $ printkeyvalue "JAVA8_HOME"
+  putStrLn $ printkeyvalue "GRAALVM_HOME"
+  putStrLn $ printkeyvalue "OPENJ9_HOME"
+  putStrLn $ printkeyvalue "ITERATIONS"
   putStrLn "# Jar names:"
   let c = combine languages compilerTargets
   let strs = map (uncurry toCheckFile) c
   mapM_ putStrLn strs
-  let x = map (\x' -> toExecution x' 500) $ combine flattenedtargets programs
+  let x = combine flattenedtargets programs
   putStrLn "# Executions:"
-  mapM_ putStrLn x
+  mapM_ (putStrLn .  toExecution) x
   let total = 500 * length x
   putStrLn $ "# Total executions " ++ show total
+  putStrLn $ printResults x
---
+++ plzoo/benchmarks.scripts/benchplan.sh
@@ -0,0 +1,289 @@
+if [ -z ${JAVA8_HOME+x} ]; then JAVA8_HOME=$PWD/java-8-oracle; fi
+if [ -z ${GRAALVM_HOME+x} ]; then GRAALVM_HOME=$PWD/graalvm-ee-1.0.0-rc7; fi
+if [ -z ${OPENJ9_HOME+x} ]; then OPENJ9_HOME=$PWD/openj9-9-0.11.0; fi
+if [ -z ${ITERATIONS+x} ]; then ITERATIONS=100; fi
+if [ ! -d "$JAVA8_HOME" ]; then echo JAVA8_HOME=$JAVA8_HOME does not exist; exit 1 ; fi
+echo SUMMARY:
+echo -n JAVA8_HOME=; echo $JAVA8_HOME
+echo -n GRAALVM_HOME=; echo $GRAALVM_HOME
+echo -n OPENJ9_HOME=; echo $OPENJ9_HOME
+echo -n ITERATIONS=; echo $ITERATIONS
+# Jar names:
+if [ ! -f "logo_visitor.jar" ]; then
+  echo "File logo_visitor.jar does not exist."
+  exit 1
+fi
+
+if [ ! -f "logo_revisitor.jar" ]; then
+  echo "File logo_revisitor.jar does not exist."
+  exit 1
+fi
+
+if [ ! -f "logo_interpreter.jar" ]; then
+  echo "File logo_interpreter.jar does not exist."
+  exit 1
+fi
+
+if [ ! -f "logo_truffle.jar" ]; then
+  echo "File logo_truffle.jar does not exist."
+  exit 1
+fi
+
+if [ ! -f "boa_visitor.jar" ]; then
+  echo "File boa_visitor.jar does not exist."
+  exit 1
+fi
+
+if [ ! -f "boa_revisitor.jar" ]; then
+  echo "File boa_revisitor.jar does not exist."
+  exit 1
+fi
+
+if [ ! -f "boa_interpreter.jar" ]; then
+  echo "File boa_interpreter.jar does not exist."
+  exit 1
+fi
+
+if [ ! -f "boa_truffle.jar" ]; then
+  echo "File boa_truffle.jar does not exist."
+  exit 1
+fi
+
+# Executions:
+echo "visitor_graalvm_logo_fractal"
+$GRAALVM_HOME/bin/java -jar logo_visitor.jar fractal.xmi $ITERATIONS | tail -n1 > visitor_graalvm_logo_fractal.log
+
+echo "visitor_graalvm_boa_fib10"
+$GRAALVM_HOME/bin/java -jar boa_visitor.jar fib10.xmi $ITERATIONS | tail -n1 > visitor_graalvm_boa_fib10.log
+
+echo "visitor_graalvm_boa_fib20"
+$GRAALVM_HOME/bin/java -jar boa_visitor.jar fib20.xmi $ITERATIONS | tail -n1 > visitor_graalvm_boa_fib20.log
+
+echo "visitor_java8_logo_fractal"
+$JAVA8_HOME/bin/java -jar logo_visitor.jar fractal.xmi $ITERATIONS | tail -n1 > visitor_java8_logo_fractal.log
+
+echo "visitor_java8_boa_fib10"
+$JAVA8_HOME/bin/java -jar boa_visitor.jar fib10.xmi $ITERATIONS | tail -n1 > visitor_java8_boa_fib10.log
+
+echo "visitor_java8_boa_fib20"
+$JAVA8_HOME/bin/java -jar boa_visitor.jar fib20.xmi $ITERATIONS | tail -n1 > visitor_java8_boa_fib20.log
+
+echo "visitor_openj9_logo_fractal"
+$OPENJ9_HOME/bin/java -jar logo_visitor.jar fractal.xmi $ITERATIONS | tail -n1 > visitor_openj9_logo_fractal.log
+
+echo "visitor_openj9_boa_fib10"
+$OPENJ9_HOME/bin/java -jar boa_visitor.jar fib10.xmi $ITERATIONS | tail -n1 > visitor_openj9_boa_fib10.log
+
+echo "visitor_openj9_boa_fib20"
+$OPENJ9_HOME/bin/java -jar boa_visitor.jar fib20.xmi $ITERATIONS | tail -n1 > visitor_openj9_boa_fib20.log
+
+echo "revisitor_graalvm_logo_fractal"
+$GRAALVM_HOME/bin/java -jar logo_revisitor.jar fractal.xmi $ITERATIONS | tail -n1 > revisitor_graalvm_logo_fractal.log
+
+echo "revisitor_graalvm_boa_fib10"
+$GRAALVM_HOME/bin/java -jar boa_revisitor.jar fib10.xmi $ITERATIONS | tail -n1 > revisitor_graalvm_boa_fib10.log
+
+echo "revisitor_graalvm_boa_fib20"
+$GRAALVM_HOME/bin/java -jar boa_revisitor.jar fib20.xmi $ITERATIONS | tail -n1 > revisitor_graalvm_boa_fib20.log
+
+echo "revisitor_java8_logo_fractal"
+$JAVA8_HOME/bin/java -jar logo_revisitor.jar fractal.xmi $ITERATIONS | tail -n1 > revisitor_java8_logo_fractal.log
+
+echo "revisitor_java8_boa_fib10"
+$JAVA8_HOME/bin/java -jar boa_revisitor.jar fib10.xmi $ITERATIONS | tail -n1 > revisitor_java8_boa_fib10.log
+
+echo "revisitor_java8_boa_fib20"
+$JAVA8_HOME/bin/java -jar boa_revisitor.jar fib20.xmi $ITERATIONS | tail -n1 > revisitor_java8_boa_fib20.log
+
+echo "revisitor_openj9_logo_fractal"
+$OPENJ9_HOME/bin/java -jar logo_revisitor.jar fractal.xmi $ITERATIONS | tail -n1 > revisitor_openj9_logo_fractal.log
+
+echo "revisitor_openj9_boa_fib10"
+$OPENJ9_HOME/bin/java -jar boa_revisitor.jar fib10.xmi $ITERATIONS | tail -n1 > revisitor_openj9_boa_fib10.log
+
+echo "revisitor_openj9_boa_fib20"
+$OPENJ9_HOME/bin/java -jar boa_revisitor.jar fib20.xmi $ITERATIONS | tail -n1 > revisitor_openj9_boa_fib20.log
+
+echo "interpreter_graalvm_logo_fractal"
+$GRAALVM_HOME/bin/java -jar logo_interpreter.jar fractal.xmi $ITERATIONS | tail -n1 > interpreter_graalvm_logo_fractal.log
+
+echo "interpreter_graalvm_boa_fib10"
+$GRAALVM_HOME/bin/java -jar boa_interpreter.jar fib10.xmi $ITERATIONS | tail -n1 > interpreter_graalvm_boa_fib10.log
+
+echo "interpreter_graalvm_boa_fib20"
+$GRAALVM_HOME/bin/java -jar boa_interpreter.jar fib20.xmi $ITERATIONS | tail -n1 > interpreter_graalvm_boa_fib20.log
+
+echo "interpreter_java8_logo_fractal"
+$JAVA8_HOME/bin/java -jar logo_interpreter.jar fractal.xmi $ITERATIONS | tail -n1 > interpreter_java8_logo_fractal.log
+
+echo "interpreter_java8_boa_fib10"
+$JAVA8_HOME/bin/java -jar boa_interpreter.jar fib10.xmi $ITERATIONS | tail -n1 > interpreter_java8_boa_fib10.log
+
+echo "interpreter_java8_boa_fib20"
+$JAVA8_HOME/bin/java -jar boa_interpreter.jar fib20.xmi $ITERATIONS | tail -n1 > interpreter_java8_boa_fib20.log
+
+echo "interpreter_openj9_logo_fractal"
+$OPENJ9_HOME/bin/java -jar logo_interpreter.jar fractal.xmi $ITERATIONS | tail -n1 > interpreter_openj9_logo_fractal.log
+
+echo "interpreter_openj9_boa_fib10"
+$OPENJ9_HOME/bin/java -jar boa_interpreter.jar fib10.xmi $ITERATIONS | tail -n1 > interpreter_openj9_boa_fib10.log
+
+echo "interpreter_openj9_boa_fib20"
+$OPENJ9_HOME/bin/java -jar boa_interpreter.jar fib20.xmi $ITERATIONS | tail -n1 > interpreter_openj9_boa_fib20.log
+
+echo "switch_graalvm_logo_fractal"
+$GRAALVM_HOME/bin/java -jar logo_switch.jar fractal.xmi $ITERATIONS | tail -n1 > switch_graalvm_logo_fractal.log
+
+echo "switch_graalvm_boa_fib10"
+$GRAALVM_HOME/bin/java -jar boa_switch.jar fib10.xmi $ITERATIONS | tail -n1 > switch_graalvm_boa_fib10.log
+
+echo "switch_graalvm_boa_fib20"
+$GRAALVM_HOME/bin/java -jar boa_switch.jar fib20.xmi $ITERATIONS | tail -n1 > switch_graalvm_boa_fib20.log
+
+echo "switch_java8_logo_fractal"
+$JAVA8_HOME/bin/java -jar logo_switch.jar fractal.xmi $ITERATIONS | tail -n1 > switch_java8_logo_fractal.log
+
+echo "switch_java8_boa_fib10"
+$JAVA8_HOME/bin/java -jar boa_switch.jar fib10.xmi $ITERATIONS | tail -n1 > switch_java8_boa_fib10.log
+
+echo "switch_java8_boa_fib20"
+$JAVA8_HOME/bin/java -jar boa_switch.jar fib20.xmi $ITERATIONS | tail -n1 > switch_java8_boa_fib20.log
+
+echo "switch_openj9_logo_fractal"
+$OPENJ9_HOME/bin/java -jar logo_switch.jar fractal.xmi $ITERATIONS | tail -n1 > switch_openj9_logo_fractal.log
+
+echo "switch_openj9_boa_fib10"
+$OPENJ9_HOME/bin/java -jar boa_switch.jar fib10.xmi $ITERATIONS | tail -n1 > switch_openj9_boa_fib10.log
+
+echo "switch_openj9_boa_fib20"
+$OPENJ9_HOME/bin/java -jar boa_switch.jar fib20.xmi $ITERATIONS | tail -n1 > switch_openj9_boa_fib20.log
+
+echo "truffle_graalvmtruffle_logo_fractal"
+$GRAALVM_HOME/bin/java -XX:-UseJVMCIClassLoader -jar logo_truffle.jar fractal.xmi $ITERATIONS | tail -n1 > truffle_graalvmtruffle_logo_fractal.log
+
+echo "truffle_graalvmtruffle_boa_fib10"
+$GRAALVM_HOME/bin/java -XX:-UseJVMCIClassLoader -jar boa_truffle.jar fib10.xmi $ITERATIONS | tail -n1 > truffle_graalvmtruffle_boa_fib10.log
+
+echo "truffle_graalvmtruffle_boa_fib20"
+$GRAALVM_HOME/bin/java -XX:-UseJVMCIClassLoader -jar boa_truffle.jar fib20.xmi $ITERATIONS | tail -n1 > truffle_graalvmtruffle_boa_fib20.log
+
+# Total executions 19500
+echo -n "visitor_graalvm_logo_fractal="
+tail -n1 visitor_graalvm_logo_fractal.log
+
+echo -n "visitor_graalvm_boa_fib10="
+tail -n1 visitor_graalvm_boa_fib10.log
+
+echo -n "visitor_graalvm_boa_fib20="
+tail -n1 visitor_graalvm_boa_fib20.log
+
+echo -n "visitor_java8_logo_fractal="
+tail -n1 visitor_java8_logo_fractal.log
+
+echo -n "visitor_java8_boa_fib10="
+tail -n1 visitor_java8_boa_fib10.log
+
+echo -n "visitor_java8_boa_fib20="
+tail -n1 visitor_java8_boa_fib20.log
+
+echo -n "visitor_openj9_logo_fractal="
+tail -n1 visitor_openj9_logo_fractal.log
+
+echo -n "visitor_openj9_boa_fib10="
+tail -n1 visitor_openj9_boa_fib10.log
+
+echo -n "visitor_openj9_boa_fib20="
+tail -n1 visitor_openj9_boa_fib20.log
+
+echo -n "revisitor_graalvm_logo_fractal="
+tail -n1 revisitor_graalvm_logo_fractal.log
+
+echo -n "revisitor_graalvm_boa_fib10="
+tail -n1 revisitor_graalvm_boa_fib10.log
+
+echo -n "revisitor_graalvm_boa_fib20="
+tail -n1 revisitor_graalvm_boa_fib20.log
+
+echo -n "revisitor_java8_logo_fractal="
+tail -n1 revisitor_java8_logo_fractal.log
+
+echo -n "revisitor_java8_boa_fib10="
+tail -n1 revisitor_java8_boa_fib10.log
+
+echo -n "revisitor_java8_boa_fib20="
+tail -n1 revisitor_java8_boa_fib20.log
+
+echo -n "revisitor_openj9_logo_fractal="
+tail -n1 revisitor_openj9_logo_fractal.log
+
+echo -n "revisitor_openj9_boa_fib10="
+tail -n1 revisitor_openj9_boa_fib10.log
+
+echo -n "revisitor_openj9_boa_fib20="
+tail -n1 revisitor_openj9_boa_fib20.log
+
+echo -n "interpreter_graalvm_logo_fractal="
+tail -n1 interpreter_graalvm_logo_fractal.log
+
+echo -n "interpreter_graalvm_boa_fib10="
+tail -n1 interpreter_graalvm_boa_fib10.log
+
+echo -n "interpreter_graalvm_boa_fib20="
+tail -n1 interpreter_graalvm_boa_fib20.log
+
+echo -n "interpreter_java8_logo_fractal="
+tail -n1 interpreter_java8_logo_fractal.log
+
+echo -n "interpreter_java8_boa_fib10="
+tail -n1 interpreter_java8_boa_fib10.log
+
+echo -n "interpreter_java8_boa_fib20="
+tail -n1 interpreter_java8_boa_fib20.log
+
+echo -n "interpreter_openj9_logo_fractal="
+tail -n1 interpreter_openj9_logo_fractal.log
+
+echo -n "interpreter_openj9_boa_fib10="
+tail -n1 interpreter_openj9_boa_fib10.log
+
+echo -n "interpreter_openj9_boa_fib20="
+tail -n1 interpreter_openj9_boa_fib20.log
+
+echo -n "switch_graalvm_logo_fractal="
+tail -n1 switch_graalvm_logo_fractal.log
+
+echo -n "switch_graalvm_boa_fib10="
+tail -n1 switch_graalvm_boa_fib10.log
+
+echo -n "switch_graalvm_boa_fib20="
+tail -n1 switch_graalvm_boa_fib20.log
+
+echo -n "switch_java8_logo_fractal="
+tail -n1 switch_java8_logo_fractal.log
+
+echo -n "switch_java8_boa_fib10="
+tail -n1 switch_java8_boa_fib10.log
+
+echo -n "switch_java8_boa_fib20="
+tail -n1 switch_java8_boa_fib20.log
+
+echo -n "switch_openj9_logo_fractal="
+tail -n1 switch_openj9_logo_fractal.log
+
+echo -n "switch_openj9_boa_fib10="
+tail -n1 switch_openj9_boa_fib10.log
+
+echo -n "switch_openj9_boa_fib20="
+tail -n1 switch_openj9_boa_fib20.log
+
+echo -n "truffle_graalvmtruffle_logo_fractal="
+tail -n1 truffle_graalvmtruffle_logo_fractal.log
+
+echo -n "truffle_graalvmtruffle_boa_fib10="
+tail -n1 truffle_graalvmtruffle_boa_fib10.log
+
+echo -n "truffle_graalvmtruffle_boa_fib20="
+tail -n1 truffle_graalvmtruffle_boa_fib20.log
+
+echo "results = {\"visitor_graalvm_logo_fractal\": visitor_graalvm_logo_fractal , \"visitor_graalvm_boa_fib10\": visitor_graalvm_boa_fib10 , \"visitor_graalvm_boa_fib20\": visitor_graalvm_boa_fib20 , \"visitor_java8_logo_fractal\": visitor_java8_logo_fractal , \"visitor_java8_boa_fib10\": visitor_java8_boa_fib10 , \"visitor_java8_boa_fib20\": visitor_java8_boa_fib20 , \"visitor_openj9_logo_fractal\": visitor_openj9_logo_fractal , \"visitor_openj9_boa_fib10\": visitor_openj9_boa_fib10 , \"visitor_openj9_boa_fib20\": visitor_openj9_boa_fib20 , \"revisitor_graalvm_logo_fractal\": revisitor_graalvm_logo_fractal , \"revisitor_graalvm_boa_fib10\": revisitor_graalvm_boa_fib10 , \"revisitor_graalvm_boa_fib20\": revisitor_graalvm_boa_fib20 , \"revisitor_java8_logo_fractal\": revisitor_java8_logo_fractal , \"revisitor_java8_boa_fib10\": revisitor_java8_boa_fib10 , \"revisitor_java8_boa_fib20\": revisitor_java8_boa_fib20 , \"revisitor_openj9_logo_fractal\": revisitor_openj9_logo_fractal , \"revisitor_openj9_boa_fib10\": revisitor_openj9_boa_fib10 , \"revisitor_openj9_boa_fib20\": revisitor_openj9_boa_fib20 , \"interpreter_graalvm_logo_fractal\": interpreter_graalvm_logo_fractal , \"interpreter_graalvm_boa_fib10\": interpreter_graalvm_boa_fib10 , \"interpreter_graalvm_boa_fib20\": interpreter_graalvm_boa_fib20 , \"interpreter_java8_logo_fractal\": interpreter_java8_logo_fractal , \"interpreter_java8_boa_fib10\": interpreter_java8_boa_fib10 , \"interpreter_java8_boa_fib20\": interpreter_java8_boa_fib20 , \"interpreter_openj9_logo_fractal\": interpreter_openj9_logo_fractal , \"interpreter_openj9_boa_fib10\": interpreter_openj9_boa_fib10 , \"interpreter_openj9_boa_fib20\": interpreter_openj9_boa_fib20 , \"switch_graalvm_logo_fractal\": switch_graalvm_logo_fractal , \"switch_graalvm_boa_fib10\": switch_graalvm_boa_fib10 , \"switch_graalvm_boa_fib20\": switch_graalvm_boa_fib20 , \"switch_java8_logo_fractal\": switch_java8_logo_fractal , \"switch_java8_boa_fib10\": switch_java8_boa_fib10 , \"switch_java8_boa_fib20\": switch_java8_boa_fib20 , \"switch_openj9_logo_fractal\": switch_openj9_logo_fractal , \"switch_openj9_boa_fib10\": switch_openj9_boa_fib10 , \"switch_openj9_boa_fib20\": switch_openj9_boa_fib20 , \"truffle_graalvmtruffle_logo_fractal\": truffle_graalvmtruffle_logo_fractal , \"truffle_graalvmtruffle_boa_fib10\": truffle_graalvmtruffle_boa_fib10 , \"truffle_graalvmtruffle_boa_fib20\": truffle_graalvmtruffle_boa_fib20}"
+
