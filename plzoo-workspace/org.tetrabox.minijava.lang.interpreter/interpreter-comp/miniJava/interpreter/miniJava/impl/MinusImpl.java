9f757e46473dc193e93e0a4b57558da009a7ff14
manuel <manuel.leduc@gmail.com>
2018-12-20 11:24:23 +0100

    2 unit test error remaining

--- plzoo-workspace/org.tetrabox.minijava.lang.interpreter/interpreter-comp/miniJava/interpreter/miniJava/impl/MinusImpl.java
+++ plzoo-workspace/org.tetrabox.minijava.lang.interpreter/interpreter-comp/miniJava/interpreter/miniJava/impl/MinusImpl.java
@@ -4,6 +4,8 @@ import java.lang.Object;
 import miniJava.interpreter.miniJava.Expression;
 import miniJava.interpreter.miniJava.MiniJavaPackage;
 import miniJava.interpreter.miniJava.Minus;
+import miniJava.interpreter.miniJava.State;
+import miniJava.interpreter.miniJava.Value;
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.ecore.EClass;
@@ -131,5 +133,28 @@ public class MinusImpl extends ExpressionImpl implements Minus {
     	return basicSetRight(null, msgs);
     }
     return super.eInverseRemove(otherEnd, featureID, msgs);
+  }
+
+  public Value evaluateExpression(State state) {
+    Value result;
+    miniJava.interpreter.miniJava.Value left = ((miniJava.interpreter.miniJava.Value)this.left.evaluateExpression(state));
+        miniJava.interpreter.miniJava.Value right = ((miniJava.interpreter.miniJava.Value)this.right.evaluateExpression(state));
+        if(left instanceof miniJava.interpreter.miniJava.IntegerValue) {
+          if(right instanceof miniJava.interpreter.miniJava.IntegerValue) {
+            miniJava.interpreter.miniJava.IntegerValue bleft = ((miniJava.interpreter.miniJava.IntegerValue)left);
+            miniJava.interpreter.miniJava.IntegerValue bright = ((miniJava.interpreter.miniJava.IntegerValue)right);
+            miniJava.interpreter.miniJava.IntegerValue tmp = ((miniJava.interpreter.miniJava.IntegerValue)miniJava.interpreter.miniJava.MiniJavaFactory.eINSTANCE.createIntegerValue());
+            tmp.setValue((bleft.getValue()) - (bright.getValue()));
+            result = tmp;
+          }
+          else {
+            result = null;
+          }
+        }
+        else {
+          result = null;
+        }
+        ;
+    return result;
   }
 }
