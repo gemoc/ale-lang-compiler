2c3499a9f1d29b37c50d38427b5a2fc0114dc416
manuel <manuel.leduc@gmail.com>
2018-12-20 11:21:27 +0100

    4 unit test error remaining

--- plzoo-workspace/org.tetrabox.minijava.lang.interpreter/interpreter-comp/miniJava/interpreter/miniJava/Null.java
+++ plzoo-workspace/org.tetrabox.minijava.lang.interpreter/interpreter-comp/miniJava/interpreter/miniJava/Null.java
@@ -3,4 +3,5 @@ package miniJava.interpreter.miniJava;
 import org.eclipse.emf.ecore.EObject;
 
 public interface Null extends EObject, Expression {
+  Value evaluateExpression(State state);
 }
--- plzoo-workspace/org.tetrabox.minijava.lang.interpreter/interpreter-comp/miniJava/interpreter/miniJava/impl/NullImpl.java
+++ plzoo-workspace/org.tetrabox.minijava.lang.interpreter/interpreter-comp/miniJava/interpreter/miniJava/impl/NullImpl.java
@@ -3,6 +3,8 @@ package miniJava.interpreter.miniJava.impl;
 import java.lang.Object;
 import miniJava.interpreter.miniJava.MiniJavaPackage;
 import miniJava.interpreter.miniJava.Null;
+import miniJava.interpreter.miniJava.State;
+import miniJava.interpreter.miniJava.Value;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
@@ -44,5 +46,12 @@ public class NullImpl extends ExpressionImpl implements Null {
     switch(featureID) {
     }
     return super.eInverseRemove(otherEnd, featureID, msgs);
+  }
+
+  public Value evaluateExpression(State state) {
+    Value result;
+    result = miniJava.interpreter.miniJava.MiniJavaFactory.eINSTANCE.createNullValue();
+        ;
+    return result;
   }
 }
